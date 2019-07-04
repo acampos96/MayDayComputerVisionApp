08cc067030d876f1290ea40bb242810da3cd0bd6
Hector Campos <h.alecampos96@gmail.com>
2019-07-03 23:44:59 -0600

    funcionalidad a botones y paso de variable
    añade funcionalidad correcta a botones "volver" y pasa el producto reconocido al intent de producto

--- app/src/main/java/com/example/maydaykt/products.kt
+++ app/src/main/java/com/example/maydaykt/products.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_products.*
 
 class products : AppCompatActivity() {
     val db=FirebaseFirestore.getInstance()
@@ -15,6 +16,12 @@ class products : AppCompatActivity() {
         var id = bundle!!.getString("id")
         var productos=db.collection("Products")
         var item=productos.whereEqualTo("ID",id).get()
+        productID.setText(id)
+    }
+
+    fun back(view: View){
+        val intent= Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
 
